0cb2faeb2cabdca7ceedad59b9f1098da142521b
ramadhanu27 <rhamadhanu2019@gmail.com>
2025-10-28 20:59:14 +0700

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -89,6 +89,18 @@ export default function RootLayout({
   return (
     <html lang="id" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WPB3JZMT');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        
         {/* Preconnect to external domains */}
         <link rel="preconnect" href="https://img.komiku.org" />
         <link rel="preconnect" href="https://thumbnail.komiku.org" />
@@ -128,6 +140,17 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WPB3JZMT"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         <ThemeProvider>
           <AuthProvider>
             <Analytics />
