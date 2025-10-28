7a3c50e400d5f8db0776abc525291875eccbf319
ramadhanu27 <rhamadhanu2019@gmail.com>
2025-10-28 20:49:42 +0700

    up

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, Suspense } from 'react'
 import { usePathname, useSearchParams } from 'next/navigation'
 
-export default function Analytics() {
+function AnalyticsContent() {
   const pathname = usePathname()
   const searchParams = useSearchParams()
 
@@ -17,4 +17,12 @@ export default function Analytics() {
   }, [pathname, searchParams])
 
   return null
+}
+
+export default function Analytics() {
+  return (
+    <Suspense fallback={null}>
+      <AnalyticsContent />
+    </Suspense>
+  )
 }
